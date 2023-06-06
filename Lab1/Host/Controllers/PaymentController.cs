f754681e9853d3a4bcc1b9e75c6e32da61db9121
TiepTV6 <tranvantiep2506@gmail.com>
2023-06-07 00:34:37 +0700

    cm

---
+++ Lab1/Host/Controllers/PaymentController.cs
@@ -0,0 +1,20 @@
+﻿using Application.Contracts.Dtos.Payment;
+using Application.Contracts.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Host.Controllers
+{
+    public class PaymentController : Controller
+    {
+        private readonly IVnPayService _iVnPayService;
+        public PaymentController(IVnPayService vnPayService)
+        {
+            _iVnPayService = vnPayService;
+        }
+        public IActionResult CreateUrl(PaymentInformationModel model)
+        {
+            var url = _iVnPayService.CreatePaymentUrl(model, HttpContext);
+            return View(url);
+        }
+    }
+}
